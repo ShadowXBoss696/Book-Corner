56799feeda764ee52ccb4ae75d3e11b665bbb13a
Arpan Mahanty <arpan.mahanty.007@gmail.com>
2025-12-23 02:58:24 +0530

    Add banner info

---
+++ src/main/java/org/bookcorner/api/BookCornerApplication.java
@@ -0,0 +1,12 @@
+package org.bookcorner.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookCornerApplication {
+
+    static void main(String... args) {
+        SpringApplication.run(BookCornerApplication.class, args);
+    }
+}
